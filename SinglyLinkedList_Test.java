03e449faef17a15f1186cd34569a4cce0282175b
dfloaizab <65038210+dfloaizab@users.noreply.github.com>
2021-08-25 18:29:22 -0500

    Create SinglyLinkedList_Test.java

---
+++ SinglyLinkedList_Test.java
@@ -0,0 +1,28 @@
+public class SinglyLinkedList_Test {
+
+    public static void main(String args[])
+    {
+        //create the list
+        SinglyLinkedList myFirstList = new SinglyLinkedList();
+
+        //add some nodes...
+        myFirstList.appendNode(new ListNode("Diego "));
+        System.out.println("nodo agregado al final");
+        myFirstList.appendNode(new ListNode("Loaiza "));
+        System.out.println("nodo agregado al final");
+        myFirstList.appendNode(new ListNode("Buitrago "));
+        System.out.println("nodo agregado al final");
+        System.out.println("nodo agregado al inicio");
+        System.out.println("Lista de Cadenas:");
+        myFirstList.printList();
+        System.out.println("Numero de elementos: "+myFirstList.getNumElements());
+
+        System.out.println("Lista de Reales:");
+        SinglyLinkedList myFirstRealList = new SinglyLinkedList();
+        myFirstRealList.printList();
+        System.out.println("Numero de elementos: "+myFirstRealList.getNumElements());
+
+
+    }
+
+}
