8180a6d0bf922c0bbee64673106c5a60758c25e9
dfloaizab <65038210+dfloaizab@users.noreply.github.com>
2022-09-07 20:53:59 -0500

    Create Taller1_Pto3_FUCLG.java

---
+++ Taller1_Pto3_FUCLG.java
@@ -0,0 +1,29 @@
+public class Taller1_Pto3_FUCLG
+{
+ 
+  
+  
+  public static void main(String[] args)
+  {
+    
+    String testString = "Cadena de prueba";
+    Stack myStack = new Stack();
+    Queue myStack = new Stack();
+    
+    //recorrer cadena para agregar elementos a cada estructura de datos:
+    for(int i = 0; i < testString.length(); i++)
+    {
+      myStack.push(new Node(testString.charAt(i).toString()));
+      myQueue.enqueue(new Node(testString.charAt(i).toString());
+    }
+                      
+    //Imprimir el contenido de cada estructura de datos
+                      
+    //Obteniendo el contenido de cada estructura (con el pop y con el dequeue),construir una nueva cadena:
+                      
+                      
+                         
+  }
+  
+  
+}
