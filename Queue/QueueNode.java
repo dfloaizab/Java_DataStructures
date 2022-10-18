219a53772d6fb781649885f019b649f6c76785dd
dfloaizab <65038210+dfloaizab@users.noreply.github.com>
2022-10-17 21:15:25 -0500

    Create QueueNode.java

---
+++ Queue/QueueNode.java
@@ -0,0 +1,30 @@
+public class QueueNode<T> {
+
+     /* atributos de un nodo */
+     T data;
+     QueueNode next;
+
+
+     /* métodos de un nodo */
+     public QueueNode(T data) {
+          this.data = data;
+     }
+
+     /* métodos get/set (consulta, modificación) */
+
+     public T getData() {
+          return data;
+     }
+
+     public void setData(T data) {
+          this.data = data;
+     }
+
+     public QueueNode getNext() {
+          return next;
+     }
+
+     public void setNext(QueueNode next) {
+          this.next = next;
+     }
+}
