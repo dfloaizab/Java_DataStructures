a3a725ed5bc4742c5027c5516b13f9f7cc252401
dfloaizab <65038210+dfloaizab@users.noreply.github.com>
2025-02-18 20:04:38 -0500

    Update OrdenamientoEnteros.java

--- 2025A/Unidad1/OrdenamientoEnteros.java
+++ 2025A/Unidad1/OrdenamientoEnteros.java
@@ -1,24 +1,25 @@
-public class OrdenamientosEnteros
-{
 
+public class OrdenamientosEnteros {
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         //declarar arreglo desordenado:
         int[] desordenado = {15, 8, 7, 9, 5, 1, 0, 4, 10, 3};
 
         //declarar arreglo que va a guardar los elementos ordenados:
-        int[] ordenado = burbuja(desordenado);
+        int[] ordenado = insertSort(desordenado);
 
         //Ordenar:
-        for(int i=0; i < ordenado.length; i++)
-            System.out.printf("%d,",ordenado[i]);
+        for (int i = 0; i < ordenado.length; i++) {
+            System.out.printf("%d,", ordenado[i]);
+        }
 
     }
 
     /**
      * Ordenamiento burbuja de números enteros
-     * @param arr, que corresponde a un arreglo de enteros donde los elementos pueden estar desordenados
+     *
+     * @param arr, que corresponde a un arreglo de enteros donde los elementos
+     * pueden estar desordenados
      * @return el arreglo ordenado en orden ascendente
      */
     public static int[] burbuja(int[] arr) {
@@ -38,16 +39,25 @@ public class OrdenamientosEnteros
         return ordenado;
     }
 
-    public static int[] insertSort(int[] arr)
-    {
+    /**
+     * TERMINAR DE IMPLEMENTAR INSERTION SORT
+     *
+     * @param arr
+     * @return
+     */
+    public static int[] insertSort(int[] arr) {
+        int len = arr.length;
 
+        for (int i = 1; i < len; i++) {
+            int key = arr[i];
+            int j = i - 1;
+            while (j >= 0 && key < arr[j]) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = key;
+        }
+        return arr;
     }
-
-    //Completar implementación de acuerdo al algoritmo
-    public static int[] mergeSort(int[] arr)
-    {
-
-    }
-
 
 }
