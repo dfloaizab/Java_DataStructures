d8792d96be7d5dd27ce9c49f024020801dd7e11e
Diego Loaiza <65038210+dfloaizab@users.noreply.github.com>
2026-02-17 18:51:42 -0500

    Create PersonaComparator.java

---
+++ 2026A/Unidad1/PersonaComparator.java
@@ -0,0 +1,17 @@
+//Con la implementación de la interfaz comparable, se puede comparar solo por un atributo.
+public class Persona implements Comparable<Persona> {
+
+    private String nombre;
+    private int edad;
+
+    public Persona(String nombre, int edad) {
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+
+    @Override
+    public int compareTo(Persona otra) {
+        return Integer.compare(this.edad, otra.edad);
+    }
+}
+
