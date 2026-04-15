5f9b8776174d6bfb16478a987e7345382b04ed22
Diego Loaiza <65038210+dfloaizab@users.noreply.github.com>
2026-04-14 19:12:54 -0500

    Add Persona class for representing individuals

---
+++ 2026A/Unidad 2/BST/Persona.java
@@ -0,0 +1,15 @@
+// Clase auxiliar Persona
+class Persona {
+    String nombre;
+    int edad;
+
+    public Persona(String nombre, int edad) {
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+
+    @Override
+    public String toString() {
+        return nombre + " (" + edad + " años)";
+    }
+}
