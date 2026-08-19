bd59146d8b8c2c4b5afdf0ac03462adb9c3a2b3d
Diego Loaiza <65038210+dfloaizab@users.noreply.github.com>
2026-08-18 21:12:18 -0500

    Create Ordenamientos.java

---
+++ 2026B/Unidad 1/Ordenamientos.java
@@ -0,0 +1,33 @@
+public class Ordenamientos
+{
+
+
+    public static void main(String[] args)
+    {
+        System.out.print("Hola, Mundo. Hoy haremos algoritmos de ordenamiento.");
+
+
+        int[] identificadores = {8,50,12,24,3,1,12,4,28,19};
+        int[] ordenado = new int[10];
+
+        //Cómo lo ordenamos? (con uno o dos ciclos):
+        //1. recorrer el arreglo desde el inicio....
+        //2. comparar...
+        //3. ...
+
+        //ORDENAMIENTOS EFICIENTES Y ORDENAMIENTOS NO TAN EFICIENTES:
+
+        //1. ORDENAMIENTOS NO TAN EFICIENTES:
+        //1.1 Implementar Ordenamiento con burbuja:
+
+
+        //1.2 Implementar un algoritmos de ordenamiento cualquiera, más eficiente:
+
+
+        //2. Implementar búsqueda binaria.
+
+    }
+
+
+
+}
