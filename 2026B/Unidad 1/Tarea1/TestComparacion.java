ae61c89ff5f050523891d2678a7a362520e11ff9
Diego Loaiza <65038210+dfloaizab@users.noreply.github.com>
2026-09-01 21:14:26 -0500

    Add TestComparacion class for vehicle comparison
    Implement TestComparacion class to compare vehicle objects and sort them.

---
+++ 2026B/Unidad 1/Tarea1/TestComparacion.java
@@ -0,0 +1,39 @@
+
+import java.util.*;
+
+public class TestComparacion {
+
+    public static void main(String[] main)
+    {
+        //Crear un objeto de la clase vehiculo:
+        Vehiculo moto1 = new Vehiculo(2005, 25000000.0f, "Honda");
+
+        //Crear otro objeto de la clase vehiculo:
+        Vehiculo carro1 = new Vehiculo(2020, 70000000.0f, "Toyota");
+
+        Vehiculo carro2 = new Vehiculo(2026, 120000000.0f, "Toyota");
+
+        //Comparar:
+        //a. Cuál de los dos vehículos es de un modelo más reciente?
+        //(si se usa el compareTo, ya compara por modelo? o debo cambiar la implementación?)
+
+        //b. Cuál de los dos vehículos es más costoso?
+
+        //Crear lista de vehiculos y ordenar:
+        List<Vehiculo> vehiculos = new ArrayList<Vehiculo>();
+        vehiculos.add(moto1);
+        vehiculos.add(carro1);
+        vehiculos.add(carro2);
+
+        //Cuando se llama el  método sort de la clase collections
+        //usa el método de comparación natural del objeto, en este caso
+        //el que está implementado en compareTo:
+        Collections.sort(vehiculos);
+
+        //c. qué se debe cambiar para que ordene por modelo?
+
+        //d. qué método usar el sort de Collections?
+        
+    }
+    
+}
